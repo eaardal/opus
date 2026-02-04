c88450bf89e8d0803124e4ff3c9f7fa0cc1a7c98
Eirik Årdal <eirikaardal@gmail.com>
2026-02-04 13:41:20 +0100

    add unit tests for tasks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+    deps: {
+      optimizer: {
+        web: {
+          include: ["@testing-library/react"],
+        },
+      },
+    },
+  },
+});
