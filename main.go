b60dd19f953e29472252cc76382bb0b7ab8b8b44
Eirik Årdal <eirikaardal@gmail.com>
2026-04-13 12:41:36 +0200

    rename to Domino

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "Opus Task Management",
-		Width:  1024,
-		Height: 768,
+		Title:  "Domino",
+		Width:  1280,
+		Height: 900,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
